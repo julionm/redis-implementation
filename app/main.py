6fdbce985d5e93b8e16228aefe1e41dbe27d907c
Julio Negri <julioyuki@outlook.com>
2024-11-19 07:26:59 -0300

    receiving input and sending pongs

--- app/main.py
+++ app/main.py
@@ -5,8 +5,13 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
 
-    connection.sendall(b"+PONG\r\n")
-
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            continue
+        
+        if data == "PING":
+            connection.sendall("+PONG\r\n")
 
 if __name__ == "__main__":
     main()
