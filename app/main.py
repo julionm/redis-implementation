31ac2477eaa8d4a28d17eaf562c88dbcb04a9a44
Julio Negri <julioyuki@outlook.com>
2024-11-19 08:45:22 -0300

    implementing thread pooling

--- app/main.py
+++ app/main.py
@@ -1,17 +1,29 @@
 import socket  # noqa: F401
+import threading
+import concurrent.futures
 
+def create_connection_reader (connection):
+    def handle_read ():
+        while True:
+            data = connection.recv(1024)
+            
+            if not data:
+                return
+                    
+            connection.sendall(b"+PONG\r\n")
+
+    return handle_read
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
+
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=2)
 
     while True:
-        data = connection.recv(1024)
+        connection, _ = server_socket.accept()
 
-        if not data:
-            continue
-            
-        connection.sendall(b"+PONG\r\n")
+        pool.submit(create_connection_reader(connection))
+
 
 if __name__ == "__main__":
     main()
