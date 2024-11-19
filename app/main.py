96e1865265687e1c474a0c7e00c5880778200b42
Julio Negri <julioyuki@outlook.com>
2024-11-19 08:48:59 -0300

    simplifying the code

--- app/main.py
+++ app/main.py
@@ -1,28 +1,22 @@
 import socket  # noqa: F401
 import threading
-import concurrent.futures
 
-def create_connection_reader (connection):
-    def handle_read ():
-        while True:
-            data = connection.recv(1024)
-            
-            if not data:
-                return
-                    
-            connection.sendall(b"+PONG\r\n")
-
-    return handle_read
+def handle_connection (connection: socket.socket):
+    while True:
+        data = connection.recv(1024)
+        
+        if not data:
+            return
+                
+        connection.sendall(b"+PONG\r\n")
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
-    pool = concurrent.futures.ThreadPoolExecutor(max_workers=2)
-
     while True:
         connection, _ = server_socket.accept()
-
-        pool.submit(create_connection_reader(connection))
+        thread = threading.Thread(target=handle_connection, args=[connection])
+        thread.start()
 
 
 if __name__ == "__main__":
