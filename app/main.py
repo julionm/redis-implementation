5b2fd0c53f8a74a0d5586e2e692520f5cb5023e2
Julio Negri <julioyuki@outlook.com>
2024-11-19 07:29:10 -0300

    fixing sending pongs

--- app/main.py
+++ app/main.py
@@ -5,11 +5,15 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
 
+    connection.sendall("+PONG\r\n")
+
     while True:
         data = connection.recv(1024)
+
         if not data:
             continue
-        
+            
+        print("received data: {}", data)    
         if data == "PING":
             connection.sendall("+PONG\r\n")
 
