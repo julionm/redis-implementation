39a6aa39e2be818264bd2b82e2fc3e52a553cada
Julio Negri <julioyuki@outlook.com>
2024-11-19 07:35:10 -0300

    fixing pong response

--- app/main.py
+++ app/main.py
@@ -5,8 +5,6 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
 
-    connection.sendall("+PONG\r\n")
-
     while True:
         data = connection.recv(1024)
 
@@ -15,7 +13,7 @@ def main():
             
         print("received data: {}", data)    
         if data == "PING":
-            connection.sendall("+PONG\r\n")
+            connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
